a8407ed2f4d842d2da21adb09a77226cd4222840
Lucas <garcia.lucas3110@icloudd.com>
2023-06-02 21:51:09 -0400

    finished heading, added slide show

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function scrollToSection(sectionId) {
         behavior: 'smooth'
     });
 }
+
+
+// Image Slide Show
+var slides = document.getElementsByClassName("slide");
+var slideIndex = 0;
+
+function showSlide() {
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 5000); // Change slide every 2 seconds
+}
+
+showSlide();
+
+
+
+
